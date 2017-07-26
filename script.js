3cd0929bd7b8a4a12c2f1d67fc148b28e3ddfdc7
Xgor <real.xgor@hotmail.com>
2017-07-27 01:09:10 +0200

    Now more fancy

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-//alert("The neighbours!");
+//var canvas = document.getElementById("canvas");
+//var ctx = canvas.getContext("2d");
+
 var themes = ["In Space", "B-ball", "Bootleg",
 	"Under the sea", "Cyberspace", "Fantasy",
 	"Cyberpunk", "Post-app", "Mat",
@@ -14,13 +16,58 @@ var games = ["Donkey Kong", "Brick break", "Minecart sequence/Endless runner",
 	"Duck hunt", "Mario Bros (Arcade)", "Ice climbers",
 	"Battlezone", "Super sprint", "Snakes N Ladders",
 	"Missile command", "Moon Patrol"];
-//var game = document.querySelector("#game");
-//var theme = document.querySelector("h1");
+
+var intervalID;
+var intervalActive = false;
+var button,themeText,gameText;
+
+function setup()
+{
+	button = document.querySelector("button");
+	themeText =document.querySelector("#theme");
+	gameText = document.querySelector("#game")
+	stopRandom()
+//	startInterval();
+}
+
+function startInterval()
+{
+	intervalActive = true;
+	intervalID = setInterval(randomize,25);
+	button.className = "btn-danger";
+	button.innerHTML = "Stop";
+	themeText.classList.remove('result');
+    gameText.classList.remove('result');
+}
+
+function stopRandom() {
+    clearInterval(intervalID);
+    button.innerHTML = "Start";
+    button.className = "btn-success";
+    themeText.classList.add('result');
+    gameText.classList.add('result');
+}
+
+function activateButton()
+{
+	if(intervalActive)
+	{
+		stopRandom();
+		intervalActive= false;
+	}
+	else
+	{
+		startInterval();
+		intervalActive = true;
+	}
+}
+
 function randomize()
 {
-	document.querySelector("#theme").innerHTML = themes[getRandomInt(0,themes.length-1)];
-	document.querySelector("#game").innerHTML = games[getRandomInt(0,games.length-1)];
+	themeText.innerHTML = themes[getRandomInt(0,themes.length-1)];
+	gameText.innerHTML = games[getRandomInt(0,games.length-1)]; 
 }
+
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
